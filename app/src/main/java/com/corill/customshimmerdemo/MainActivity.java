0668d1f60ea8ac3c6f985c14810ddcaae98b1457
serenadegx <1099805713@qq.com>
2018-09-26 23:02:35 +0800

    build1.1

--- app/src/main/java/com/corill/customshimmerdemo/MainActivity.java
+++ app/src/main/java/com/corill/customshimmerdemo/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         recyclerView = (CustomShimmerRecyclerView) findViewById(R.id.rv);
         final List<String> list = new ArrayList<>();
